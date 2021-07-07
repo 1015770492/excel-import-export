89fb62d12b152986727cf6169fb33237e0a0b1e9
jinhua <1015770492@qq.com>
2021-07-07 08:43:58 +0800

    变更字段名称

--- src/test/java/top/yumbo/test/excel/importDemo/PIMCheckNullLogic.java
+++ src/test/java/top/yumbo/test/excel/importDemo/PIMCheckNullLogic.java
@@ -20,30 +20,29 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductImmovableMortgage implements Serializable {
+public class PIMCheckNullLogic implements Serializable {
 
-    private static final long serialVersionUID = 1L;
 
     /**
      * 主键Id
      */
-    private Long immovablesmortgageId;
+    private Long IMId;
     /**
      * 产品维度（字典1289）
      */
-    private String productDimension;
+    private String pD;
     /**
      * 产品id
      */
-    private Long productId;
+    private Long PId;
     /**
      * 抵押人企业Id
      */
-    private String branchNo;
+    private String bN;
     /**
      * 抵押人/所有权人名称
      */
-    private String mortgagorIm;
+    private String mIm;
     /**
      * 抵押标的类型(字典1181)
      * 8001301	土地使用权
@@ -53,15 +52,15 @@ public class ProductImmovableMortgage implements Serializable {
     @MapEntry(key = "土地使用权", value = "8001301")
     @MapEntry(key = "不动产", value = "8001302")
     @MapEntry(key = "在建工程", value = "8001303")
-    private String mortgageSubjectTypeIm;
+    private String mSTIm;
     /**
      * 权证证号
      */
-    private String warrantNumberIm;
+    private String wNIm;
     /**
      * 坐落
      */
-    private String siteIm;
+    private String sIM;
     /**
      * 权利类型（字典1182）
      * 8001601	集体土地使用权
@@ -85,113 +84,113 @@ public class ProductImmovableMortgage implements Serializable {
     @MapEntry(key = "地役权", value = "8001608")
     @MapEntry(key = "抵押权", value = "8001609")
     @MapEntry(key = "法律规定需要登记的其他不动产权利", value = "8001610")
-    private String rightTypeIm;
+    private String rTIm;
     /**
      * 使用年限
      */
-    private String usefulLifeIm;
+    private String uLIm;
     /**
      * 用途
      */
-    private String purposeIm;
+    private String pIm;
     /**
      * 是否受限(是/否，字典1022)
      */
     @MapEntry(key = "否", value = "0")
     @MapEntry(key = "是", value = "1")
-    private String isLimitedIm;
+    private String iLIm;
     /**
      * 房屋建筑面积,(当【抵押标的类型】是房产【8001302】或在建工程【8001303】时，必填)
      */
-    @CheckNullLogic(values = {"8001302","8001303"}, follow = "mortgageSubjectTypeIm")
-    private String buildingAreaIm;
+    @CheckNullLogic(values = {"8001302","8001303"}, follow = "mSTIm")
+    private String bAIM;
     /**
      * 房屋建筑面积单位
      */
-    private String buildingAreaImUnit;
+    private String bAIMUnit;
     /**
      * 宗地面积,(当【抵押标的类型】是土地使用权【8001301】时，必填)
      */
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = "8001301")
-    private String patriarchalAreaIm;
+    @CheckNullLogic(follow = "mSTIm", values = "8001301")
+    private String pAIM;
     /**
      * 宗地面积单位
      */
-    private String patriarchalAreaImUnit;
+    private String pAIMUnit;
     /**
      * 是否国营主要物业或标志性资产(是/否，字典1022)
      */
     @MapEntry(key = "否", value = "0")
     @MapEntry(key = "是", value = "1")
-    private String isgywybzz;
+    private String iGYYWBZZ;
     /**
      * 是否有评估价值(是/否，字典1022)
      */
     @MapEntry(key = "否", value = "0")
     @MapEntry(key = "是", value = "1")
-    private String isAssessmentValueIm;
+    private String iAVIM;
     /**
      * 评估报告类型(根据字典表-项目-评估报告类型取值，【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentReportTypeIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aRTIm;
     /**
      * 评估机构(【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentMechanismIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aMIm;
     /**
      * 评估报告名称(【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentReportNameIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aRNIm;
     /**
      * 评估报告编号(【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentReportSnoIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aRSIm;
     /**
      * 评估价值，单位(【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentValueIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aVIm;
     /**
      * 评估基准日(【是否有评估价值】为是时，必填)
      */
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "1")
-    private String assessmentBaseDateIm;
+    @CheckNullLogic(follow = "iAVIM", values = "1")
+    private String aBDIm;
     /**
      * 抵押类型(根据字典表-项目-抵押类型取值)
      */
-    private String mortgageTypeIm;
+    private String mTIM;
     /**
      * 权利性质 字典表-项目-权利性质
      */
-    private String rightProp;
+    private String RP;
     /**
      * 担保范围（字典1310）
      */
-    private String guaranteeRange;
+    private String gR;
     /**
      * 担保范围描述
      */
-    private String guaranteeRangeRemark;
+    private String gRR;
     /**
      * 抵押权人类型（字典1311）
      */
-    private String mortgageHolderType;
+    private String mHT;
     /**
      * 受权抵押权公司名称
      */
-    private String mortgageEnterprise;
+    private String mE;
     /**
      * 受权抵押权公司法定代表/责任人
      */
-    private String mortgageEnterpriseInstrepr;
+    private String mEI;
     /**
      * 受权抵押权公司联系电话
      */
-    private String mortgageEnterpriseTel;
+    private String mETel;
     /**
      * 创建时间
      */
--- src/test/java/top/yumbo/test/excel/importDemo/PIMExcel.java
+++ src/test/java/top/yumbo/test/excel/importDemo/PIMExcel.java
@@ -17,19 +17,19 @@ import java.time.LocalDate;
 public class PIMExcel {
 
     @ExcelCellBind(title = "*抵押人/所有权人")
-    private String mortgagorIm;
+    private String mIm;
 
     @MapEntry(key = "土地使用权", value = "8001301")
     @MapEntry(key = "不动产", value = "8001302")
     @MapEntry(key = "在建工程", value = "8001303")
     @ExcelCellBind(title = "*抵押标的类型")
-    private String mortgageSubjectTypeIm;
+    private String mSTIm;
 
     @ExcelCellBind(title = "*权证证号")
-    private String warrantNumberIm;
+    private String wNIm;
 
     @ExcelCellBind(title = "*坐落")
-    private String siteIm;
+    private String sIM;
 
     @MapEntry(key = "1", value = "8001601")
     @MapEntry(key = "2", value = "8001602")
@@ -42,45 +42,45 @@ public class PIMExcel {
     @MapEntry(key = "9", value = "8001609")
     @MapEntry(key = "10", value = "8001610")
     @ExcelCellBind(title = "*权利类型", splitRegex = ",")
-    private String rightTypeIm;
+    private String rTIm;
 
     @ExcelCellBind(title = "*使用年限")
-    private String usefulLifeIm;
+    private String uLIm;
 
     @ExcelCellBind(title = "*用途")
-    private String purposeIm;
+    private String pIm;
 
     @ExcelCellBind(title = "*是否受限")
-    private String isLimitedIm;
+    private String iLIm;
 
 
     @ExcelCellBind(title = "*是否国营主要物业或标志性资产")
-    private String isgywybzz;
+    private String iGYYWBZZ;
 
     @ExcelCellBind(title = "*房屋建筑面积", nullable = true)
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = {"8001302", "8001303"})
-    private BigDecimal buildingAreaIm;
+    @CheckNullLogic(follow = "mSTIm", values = {"8001302", "8001303"})
+    private BigDecimal bAIM;
 
     @MapEntry(key = "公顷", value = "10000")
     @MapEntry(key = "平方公里", value = "1000000")
     @MapEntry(key = "平方米", value = "1")
     @MapEntry(key = "亩", value = "666.66667")
     @ExcelCellBind(title = "*房屋建筑面积单位", nullable = true)
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = {"8001302", "8001303"})
-    @AccountBigDecimalValue(follow = "buildingAreaIm", decimalFormat = "#.##")
-    private String buildingAreaImSize;
+    @CheckNullLogic(follow = "mSTIm", values = {"8001302", "8001303"})
+    @AccountBigDecimalValue(follow = "bAIM", decimalFormat = "#.##")
+    private String bAIMSize;
 
     @ExcelCellBind(title = "*房屋建筑面积单位", nullable = true)
     @MapEntry(key = "公顷", value = "Hectares")
     @MapEntry(key = "平方公里", value = "kilometers")
     @MapEntry(key = "平方米", value = "meters")
     @MapEntry(key = "亩", value = "acres")
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = {"8001302", "8001303"})
-    private String buildingAreaImUnit;
+    @CheckNullLogic(follow = "mSTIm", values = {"8001302", "8001303"})
+    private String bAIMUnit;
 
     @ExcelCellBind(title = "*宗地面积", nullable = true)
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = "8001301")
-    private BigDecimal patriarchalAreaIm;
+    @CheckNullLogic(follow = "mSTIm", values = "8001301")
+    private BigDecimal pAIM;
 
     // 换算成平方米,冗余字段用于单位换算，不存数据库
     @MapEntry(key = "公顷", value = "10000")
@@ -88,49 +88,49 @@ public class PIMExcel {
     @MapEntry(key = "平方米", value = "1")
     @MapEntry(key = "亩", value = "666.66667")
     @ExcelCellBind(title = "*宗地面积单位", nullable = true)
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = "8001301")
-    @AccountBigDecimalValue(follow = "patriarchalAreaIm", decimalFormat = "#.##")
-    private String patriarchalAreaImSize;
+    @CheckNullLogic(follow = "mSTIm", values = "8001301")
+    @AccountBigDecimalValue(follow = "pAIM", decimalFormat = "#.##")
+    private String pAIMSize;
 
     @MapEntry(key = "公顷", value = "Hectares")
     @MapEntry(key = "平方公里", value = "kilometers")
     @MapEntry(key = "平方米", value = "meters")
     @MapEntry(key = "亩", value = "acres")
     @ExcelCellBind(title = "*宗地面积单位", nullable = true)
-    @CheckNullLogic(follow = "mortgageSubjectTypeIm", values = "8001301")
-    private String patriarchalAreaImUnit;
+    @CheckNullLogic(follow = "mSTIm", values = "8001301")
+    private String pAIMUnit;
 
     @MapEntry(key = "共同抵押", value = "0")
     @MapEntry(key = "承诺抵押", value = "1")
     @ExcelCellBind(title = "*抵押类型")
-    private String mortgageTypeIm;
+    private String mTIM;
 
     @ExcelCellBind(title = "*是否有评估价值")
     @NotEmpty(message = "是否有评估价值不允许为空")
-    private String isAssessmentValueIm;
+    private String iAVIM;
 
 
     @ExcelCellBind(title = "*评估报告类型", nullable = true)
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
+    @CheckNullLogic(follow = "iAVIM", values = "是")
     @MapEntry(key = "预评估报告",value = "8012201")
     @MapEntry(key = "正式评估报告",value = "8012202")
-    private String assessmentReportTypeIm;
+    private String aRTIm;
 
     @ExcelCellBind(title = "*评估机构", nullable = true)
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
-    private String assessmentMechanismIm;
+    @CheckNullLogic(follow = "iAVIM", values = "是")
+    private String aMIm;
 
     @ExcelCellBind(title = "*评估报告名称", nullable = true)
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
-    private String assessmentReportNameIm;
+    @CheckNullLogic(follow = "iAVIM", values = "是")
+    private String aRNIm;
 
     @ExcelCellBind(title = "*评估报告编号", nullable = true)
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
-    private String assessmentReportSnoIm;
+    @CheckNullLogic(follow = "iAVIM", values = "是")
+    private String aRSIm;
 
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
+    @CheckNullLogic(follow = "iAVIM", values = "是")
     @ExcelCellBind(title = "*评估价值", nullable = true)
-    private BigDecimal assessmentValueIm;
+    private BigDecimal aVIm;
     // 金额单位换算
     @MapEntry(key = "元", value = "1")
     @MapEntry(key = "万", value = "10000")
@@ -138,8 +138,8 @@ public class PIMExcel {
     @MapEntry(key = "千万", value = "10000000")
     @MapEntry(key = "亿", value = "100000000")
     @ExcelCellBind(title = "*评估价值单位", nullable = true)
-    @AccountBigDecimalValue(follow = "assessmentValueIm", decimalFormat = "#.##")
-    private String assessmentValueImSize;
+    @AccountBigDecimalValue(follow = "aVIm", decimalFormat = "#.##")
+    private String aVImSize;
 
     @ExcelCellBind(title = "*评估价值单位", nullable = true)
     @MapEntry(key = "元", value = "One")
@@ -147,37 +147,37 @@ public class PIMExcel {
     @MapEntry(key = "百万", value = "Million")
     @MapEntry(key = "千万", value = "TenMillion")
     @MapEntry(key = "亿", value = "HundredMillion")
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
-    private String assessmentValueImUnit;
+    @CheckNullLogic(follow = "iAVIM", values = "是")
+    private String aVImUnit;
 
     @ExcelCellBind(title = "*评估基准日", nullable = true)
-    @CheckNullLogic(follow = "isAssessmentValueIm", values = "是")
-    private LocalDate assessmentBaseDateIm;
+    @CheckNullLogic(follow = "iAVIM", values = "是")
+    private LocalDate aBDIm;
 
     @MapEntry(key = "出让", value = "0")
     @MapEntry(key = "划拨", value = "1")
     @ExcelCellBind(title = "*权利性质")
-    private String rightProp;
+    private String RP;
 
     @MapEntry(key = "受托管理人",value = "0")
     @MapEntry(key = "受权抵押权人",value = "1")
     @ExcelCellBind(title = "*抵押权人")
-    private String mortgageHolderType;
+    private String mHT;
 
     @ExcelCellBind(title = "*受权抵押权公司名称", nullable = true)
-    private String mortgageEnterprise;
+    private String mE;
 
     @ExcelCellBind(title = "*受权抵押权公司法定代表/责任人", nullable = true)
-    private String mortgageEnterpriseInstrepr;
+    private String mEI;
 
     @ExcelCellBind(title = "*受权抵押权公司联系电话", nullable = true)
-    private String mortgageEnterpriseTel;
+    private String mETel;
 
     @ExcelCellBind(title = "*担保范围")
     @MapEntry(key = "正常", value = "0")
     @MapEntry(key = "特殊", value = "1")
-    private String guaranteeRange;
+    private String gR;
 
     @ExcelCellBind(title = "*特殊说明", nullable = true)
-    private String guaranteeRangeRemark;
+    private String gRR;
 }
